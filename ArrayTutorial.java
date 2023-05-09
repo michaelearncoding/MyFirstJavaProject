7a1f0b11929b75e5c72144ae6ed04356c9af31c9
michaelearncoding <1445972573@qq.com>
2023-05-09 20:25:16 +0800

    This is same problem with Py but in JAVA

---
+++ ArrayTutorial.java
@@ -0,0 +1,14 @@
+package michaelmai;
+class Solution {
+    public int findRepeatNumber(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        int repeat = -1;
+        for (int num : nums) {
+            if (!set.add(num)) {
+                repeat = num;
+                break;
+            }
+        }
+        return repeat;
+    }
+}
